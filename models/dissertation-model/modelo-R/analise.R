720f4fbcc260bc732329da632afaf5537a3bb08f
pedroliman <pedrolima.n@gmail.com>
2017-11-03 18:36:25 -0200

    Diffusion Sector - Ithink

--- models/dissertation-model/modelo-R/analise.R
+++ models/dissertation-model/modelo-R/analise.R
@@ -35,7 +35,6 @@ resultado_unico <- data.frame(ode(y=stocks, times=simtime, func = modelo,
 
 
 
-
 results = simularRDM_e_escolher_estrategia(inputs = "params.xlsx", sdmodel = sdmodel, opcoes = opcoes)
 
 incertezas = c("aAdvertisingEffectiveness", "aContactRate", "aAdoptionFraction", "aAdvertisingCost", "aAverageTicket")
